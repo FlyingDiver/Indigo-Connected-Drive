be995f41d897ed5c75e947bb4e9dbd8914fb345a
Joe Keenan <joe@flyingdiver.com>
2022-04-19 10:55:06 -0400

    Test release

--- ConnectedDrive.indigoPlugin/Contents/Server Plugin/plugin.py
+++ ConnectedDrive.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -35,20 +35,20 @@ def no_convert(x):
 
 status_format = {
     "us": {
-        "chargingLevelHv": (u"{}%", no_convert),
-        "doorLockState": (u"{}", no_convert),
-        "fuelPercent": (u"{}%", no_convert),
-        "mileage": (u"{:.0f} miles", km2miles),
-        "remainingFuel": (u"{:.1f} gal", liters2gallons),
-        "remainingRangeFuelMls": (u"{:.0f} mi", no_convert),
+        "charging_level_hv": (u"{}%", no_convert),
+        "door_lock_state": (u"{}", no_convert),
+        "fuel_percent": (u"{}%", no_convert),
+        "mileage": (u"{:.0f} miles", no_convert),
+        "remaining_fuel": (u"{:.1f} gal", liters2gallons),
+        "remaining_range_fuel": (u"{:.0f} mi", no_convert),
     },
     "metric": {
-        "chargingLevelHv": (u"{}%", no_convert),
-        "doorLockState": (u"{}", no_convert),
-        "fuelPercent": (u"{}%", no_convert),
+        "charging_level_hv": (u"{}%", no_convert),
+        "door_lock_state": (u"{}", no_convert),
+        "fuel_percent": (u"{}%", no_convert),
         "mileage": (u"{} km", no_convert),
-        "remainingFuel": (u"{} ltrs", no_convert),
-        "remainingRangeFuel": (u"{} km", no_convert),
+        "remaining_fuel": (u"{} ltrs", no_convert),
+        "remaining_range_fuel": (u"{} km", no_convert),
     }
 }
 
@@ -102,17 +102,21 @@ async def air_conditioning(username, password, region, vin):
     status = vehicle.remote_services.trigger_remote_air_conditioning()
     return status.state
 
-
-async def send_message(username, password, region, vin, subject, text):
-    """Send a message to car."""
+async def air_conditioning_off(username, password, region, vin):
     account = ConnectedDriveAccount(username, password, get_region_from_name(region))
     vehicle = account.get_vehicle(vin)
-    msg_data = dict(
-        text=text,
-        subject=subject
-    )
-    vehicle.remote_services.trigger_send_message(msg_data)
+    if not vehicle:
+        return None
+    status = vehicle.remote_services.trigger_remote_air_conditioning_stop()
+    return status.state
 
+async def charge_now(username, password, region, vin):
+    account = ConnectedDriveAccount(username, password, get_region_from_name(region))
+    vehicle = account.get_vehicle(vin)
+    if not vehicle:
+        return None
+    status = vehicle.remote_services.trigger_charge_nowp()
+    return status.state
 
 class Plugin(indigo.PluginBase):
 
@@ -129,7 +133,8 @@ class Plugin(indigo.PluginBase):
 
         self.updateFrequency = float(pluginPrefs.get('updateFrequency', "30")) * 60.0
         self.logger.debug(f"updateFrequency = {self.updateFrequency}")
-        self.next_update = time.time() + self.updateFrequency
+        self.next_update = time.time() + 30.0   # give time for devices to get initialized
+        self.need_update = False
 
         self.units = pluginPrefs.get('units', "us")
 
@@ -140,7 +145,6 @@ class Plugin(indigo.PluginBase):
         self.cd_accounts = {}
         self.cd_vehicles = {}
         self.vehicle_data = {}
-        self.vehicle_states = {}
 
     def startup(self):
         self.logger.info("Starting Connected Drive")
@@ -171,8 +175,9 @@ class Plugin(indigo.PluginBase):
     def runConcurrentThread(self):
         try:
             while True:
-                if time.time() > self.next_update:
+                if time.time() > self.next_update or self.need_update:
                     self.next_update = time.time() + self.updateFrequency
+                    self.need_update = False
 
                     for acctDevID in self.cd_accounts.keys():
                         self._do_update(indigo.devices[acctDevID])
@@ -192,6 +197,7 @@ class Plugin(indigo.PluginBase):
             self.logger.info(f"{device.name}: {len(self.vehicle_data)} vehicles found.")
 
         elif device.deviceTypeId == "cdVehicle":
+            self.need_update = True
             self.cd_vehicles[device.address] = device.id
 
         else:
@@ -218,14 +224,13 @@ class Plugin(indigo.PluginBase):
             del status_dict['condition_based_services']
             del status_dict['lids']
             del status_dict['windows']
+            timestamp = status_dict['timestamp'].strftime("%d %b %Y %H:%M:%S %Z")
             del status_dict['timestamp']
 
             self.vehicle_data[vehicle.vin] = {'account': cd_account.id,
                                               'vehicle': vehicle_dict,
                                               'status': status_dict,
                                               }
-
-            # If there's an Indigo device for this vehicle, update it
 
             vehicleDevID = self.cd_vehicles.get(vehicle.vin, None)
             if not vehicleDevID:
@@ -246,18 +251,28 @@ class Plugin(indigo.PluginBase):
                            {'key': 'driveTrain', 'value': vehicle.driveTrain},
                            {'key': 'all_lids_closed', 'value': vehicle.all_lids_closed},
                            {'key': 'all_windows_closed', 'value': vehicle.all_windows_closed},
-                           {'key': 'all_doors_locked', 'value': vehicle.door_lock_state == "LOCKED"},
-
+                           {'key': 'door_lock_state', 'value': vehicle.door_lock_state},
                            {'key': 'fuel_percent', 'value': vehicle.fuel_percent},
+                           {'key': 'mileage', 'value': status_dict['mileage'][0]},
+                           {'key': 'charging_level_hv', 'value': vehicle.charging_level_hv},
+                           {'key': 'gps_lat', 'value': status_dict['gps_position'][0]},
+                           {'key': 'gps_long', 'value': status_dict['gps_position'][1]},
+                           {'key': 'gps_heading', 'value': status_dict['gps_heading']},
+                           {'key': 'timestamp', 'value': timestamp},
                            ]
+            state_key = vehicleDevice.pluginProps["state_key"]
+            match state_key:
+                case 'mileage' | 'remaining_fuel' | 'remaining_range_fuel':
+                    status_value, status_units = status_dict[state_key]
+                case _:
+                    status_value = status_dict[state_key]
+                    status_units = None
 
-            self.vehicle_states[vehicleDevice.id] = states_list
-
-            try:
-                vehicleDevice.stateListOrDisplayStateIdChanged()
-                vehicleDevice.updateStatesOnServer(states_list)
-            except TypeError as err:
-                self.logger.error(f"{vehicleDevice.name}: invalid state type in states_list: {states_list}")
+            ui_format, converter = status_format[self.units][state_key]
+            if not status_value:
+                status_value = ""
+            states_list.append({'key': 'status', 'value': status_value, 'uiValue': ui_format.format(converter(status_value))})
+            vehicleDevice.updateStatesOnServer(states_list)
 
     def get_vehicle_list(self, filter="", valuesDict=None, typeId="", targetId=0):
         self.logger.threaddebug(f"get_vehicle_list: typeId = {typeId}, targetId = {targetId}, valuesDict = {valuesDict}")
@@ -277,9 +292,8 @@ class Plugin(indigo.PluginBase):
             return retList
 
         vehicle_status = self.vehicle_data[vin]['status']
-        for s in ['chargingLevelHv', 'fuelPercent', 'mileage', 'remainingFuel', 'remainingRangeFuel', 'doorLockState']:
-            if s in vehicle_status:
-                retList.append((s, s))
+        for s in ['charging_level_hv', 'fuel_percent', 'mileage', 'remaining_fuel', 'remaining_range_fuel', 'door_lock_state']:
+            retList.append((s, s))
         retList.sort(key=lambda tup: tup[1])
         return retList
 
@@ -301,3 +315,50 @@ class Plugin(indigo.PluginBase):
             self.logger.info(
                 f"Data for VIN {vin}:\n{json.dumps(self.vehicle_data[vin], skipkeys=True, sort_keys=True, indent=4, separators=(',', ': '))}")
         return True
+
+    def sendCommandAction(self, pluginAction, vehicleDevice, callerWaitingForResult):
+        self.logger.debug(f"{vehicleDevice.name}: sendCommandAction {pluginAction.props['serviceCode']} for VIN {vehicleDevice.address}")
+        cd_account = indigo.devices[int(self.vehicle_data[vehicleDevice.address]['account'])]
+        self.logger.debug(f"{vehicleDevice.name}: sendCommandAction using cd_account: {cd_account.name}")
+
+        match pluginAction.props["serviceCode"]:
+            case 'light':
+                ret = asyncio.run(light_flash(cd_account.pluginProps['username'], cd_account.pluginProps['password'], cd_account.pluginProps['region'],
+                                vehicleDevice.address))
+                self.logger.debug(f"{vehicleDevice.name}: sendCommandAction {pluginAction.props['serviceCode']} result: {ret}")
+
+            case 'lock':
+                ret = asyncio.run(door_lock(cd_account.pluginProps['username'], cd_account.pluginProps['password'], cd_account.pluginProps['region'],
+                                vehicleDevice.address))
+                self.logger.debug(f"{vehicleDevice.name}: sendCommandAction {pluginAction.props['serviceCode']} result: {ret}")
+
+            case 'unlock':
+                ret = asyncio.run(door_unlock(cd_account.pluginProps['username'], cd_account.pluginProps['password'], cd_account.pluginProps['region'],
+                              vehicleDevice.address))
+                self.logger.debug(f"{vehicleDevice.name}: sendCommandAction {pluginAction.props['serviceCode']} result: {ret}")
+
+            case 'horn':
+                ret = asyncio.run(horn(cd_account.pluginProps['username'], cd_account.pluginProps['password'], cd_account.pluginProps['region'],
+                                vehicleDevice.address))
+                self.logger.debug(f"{vehicleDevice.name}: sendCommandAction {pluginAction.props['serviceCode']} result: {ret}")
+
+            case 'climate':
+                ret = asyncio.run(air_conditioning(cd_account.pluginProps['username'], cd_account.pluginProps['password'], cd_account.pluginProps['region'],
+                                vehicleDevice.address))
+                self.logger.debug(f"{vehicleDevice.name}: sendCommandAction {pluginAction.props['serviceCode']} result: {ret}")
+
+            case 'climate_off':
+                ret = asyncio.run(air_conditioning_off(cd_account.pluginProps['username'], cd_account.pluginProps['password'], cd_account.pluginProps['region'],
+                                vehicleDevice.address))
+                self.logger.debug(f"{vehicleDevice.name}: sendCommandAction {pluginAction.props['serviceCode']} result: {ret}")
+
+            case 'charge_now':
+                ret = asyncio.run(charge_now(cd_account.pluginProps['username'], cd_account.pluginProps['password'], cd_account.pluginProps['region'],
+                                vehicleDevice.address))
+                self.logger.debug(f"{vehicleDevice.name}: sendCommandAction {pluginAction.props['serviceCode']} result: {ret}")
+
+            case _:
+                self.logger.warning(f"{vehicleDevice.name}: sendCommandAction unknown serviceCode: {serviceCode}")
+
+        # schedule an update shortly
+        self.next_update = time.time() + 30.0
